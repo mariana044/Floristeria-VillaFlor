7466dda42099e877ed1161eea859ec0487ad2263
kaaraya212 <katheaarine@gmail.com>
2025-10-31 21:23:43 -0600

    Testing
    HomeController: Recibe solicitudes HTTP y devuelve la vista que debe mostrar. Es donde defino la lógica para manejar las páginas de inicio, login, registro, etc.
    
    Login.html: Define la página que verá el usuario al acceder a /login.
    Base.html: Define la base para mis otras páginas: navbar, footer, CSS, scripts.

---
+++ ProyectoFlor/src/test/java/com/ProyectoFlor/ProyectoFlorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProyectoFlor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFlorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
