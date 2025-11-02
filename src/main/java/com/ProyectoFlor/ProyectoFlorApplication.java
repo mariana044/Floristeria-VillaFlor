304c6e5825c93b551d2279979cd88d45be953c45
dquesada90259 <145231337+dquesada90259@users.noreply.github.com>
2025-11-02 10:20:53 -0600

    P1
    Se devuelve a pagina de inicio
    Cambio de nombres de los de menu
    Agregue pagina de admin
    Footer cambiado

---
+++ src/main/java/com/ProyectoFlor/ProyectoFlorApplication.java
@@ -0,0 +1,13 @@
+package com.ProyectoFlor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFlorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoFlorApplication.class, args);
+    }
+
+}
